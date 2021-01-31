fc7e6d6f788257172f2308228bc9eddc813b5447
Dainel de Souza Neris <danielneris01@gmail.com>
2021-01-31 19:44:19 -0300

    add serializers

---
+++ courses/api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import Course, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        model = Review
+        fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
