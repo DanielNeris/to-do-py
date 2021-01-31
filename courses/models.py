db630e61ece25177c0928831a151a596aff810bd
Dainel de Souza Neris <danielneris01@gmail.com>
2021-01-31 19:08:54 -0300

    make migrations

--- courses/models.py
+++ courses/models.py
@@ -8,7 +8,7 @@ class Base(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
 
-    class Meda:
+    class Meta:
         abstract = True
 
 
@@ -20,20 +20,22 @@ class Course(Base):
         verbose_name = 'Course'
         verbose_name_plural = 'Courses'
 
-      def __str__(self):
+    def __str__(self):
         return self.title
 
+
 class Review(Base):
-  course = models.ForeignKey(Courses, related_name='reviews', on_delete=models.CASCADE)
-  name = models.CharField(max_length=255)
-  email = models.EmailField()
-  comment = models.TextField(blank=True, default='')
-  review = models.DecimalField(max_digits=2, decimal_places=1)
+    course = models.ForeignKey(
+        Course, related_name='reviews', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    comment = models.TextField(blank=True, default='')
+    review = models.DecimalField(max_digits=2, decimal_places=1)
 
-  class Meta:
-      verbose_name = 'Review'
-      verbose_name_plural = 'Reviews'
-      unique_togther = ['email', 'course']
+    class Meta:
+        verbose_name = 'Review'
+        verbose_name_plural = 'Reviews'
+        unique_together = ['email', 'course']
 
-      def __str__(self):
+    def __str__(self):
         return f'{self.name} review the course {self.course} with note {self.review}'
