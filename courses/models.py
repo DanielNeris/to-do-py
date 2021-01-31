80ae9369f2b1739e3ea0ef83df29197a51137b31
Dainel de Souza Neris <danielneris01@gmail.com>
2021-01-31 18:41:58 -0300

    set base model

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meda:
+        abstract = True
